388ebe4d03094b78f4cfcdc89219b596728ef6ca
EvelioVallarta24 <91225966+EvelioVallarta24@users.noreply.github.com>
2022-01-31 17:40:41 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,6 +7,9 @@ var form, game, player;
 function setup() {
   canvas = createCanvas(400,400);
   database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
 
 function draw() {
